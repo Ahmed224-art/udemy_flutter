9bf752de458b547beba48f7e3ea9e783839cf4e6
talenta <2n22hmedh2ss2n@gmail.com>
2023-01-17 22:16:12 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp();
+}
+class Myapp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+    home: ,
+  );
+  }
+
+}
